19eb9e11743a4d32250a10a95a2bd6318478b703
Jatin Saxena <jatinsaxena@Jatins-MacBook-Pro.local>
2021-11-26 23:15:56 -0600

    Scoring implementation

--- code/parsing-engine/src/main/java/analysisengine/ScoringEngine.java
+++ code/parsing-engine/src/main/java/analysisengine/ScoringEngine.java
@@ -4,25 +4,23 @@ import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.core.SimpleAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
+import org.apache.lucene.document.IntField;
 import org.apache.lucene.document.StringField;
-import org.apache.lucene.document.TextField;
 import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
-import org.apache.lucene.queryparser.classic.QueryParser;
-import org.apache.lucene.search.IndexSearcher;
-import org.apache.lucene.search.Query;
-import org.apache.lucene.search.ScoreDoc;
-import org.apache.lucene.search.TopScoreDocCollector;
+import org.apache.lucene.search.*;
 import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.util.Version;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -38,7 +36,6 @@ public class ScoringEngine {
         FSDirectory dir = FSDirectory.open(new File(getClass().getResource("/CORPUS").getFile()));
         IndexWriterConfig config = new IndexWriterConfig(Version.LUCENE_47,analyzer);
         writer = new IndexWriter(dir, config);
-        //TODO - Implement Similarity
     }
 
     public void indexFilesDirectory() throws IOException {
@@ -47,26 +44,52 @@ public class ScoringEngine {
 
         int OriginalNumDocs = writer.numDocs();
 
+
+
         queue.forEach(file -> {
-            FileReader fr = null;
             try {
-                fr = new FileReader(file);
+
+                FileReader fr = new FileReader(file);
+                Object obj = new JSONParser().parse(fr);
+                JSONObject jo = (JSONObject) obj;
+                String location = (String) jo.get("location");
+                JSONArray ja = (JSONArray) jo.get("skills");
+                String allSkills = " ";
+                Iterator itr2 = ja.iterator();
                 Document document = new Document();
-                //Add content from json file
-                document.add(new TextField("contents",fr));
                 document.add(new StringField("path", file.getPath(), Field.Store.YES));
                 document.add(new StringField("filename", file.getName(), Field.Store.YES));
-                writer.addDocument(document);
-            } catch (IOException e) {
+                document.add(new StringField("location", location, Field.Store.YES));
+                while (itr2.hasNext()) {
+                    Iterator<Map.Entry> itr1 = ((Map) itr2.next()).entrySet().iterator();
+                    int duration=0;
+                    String skill = null;
+                    while (itr1.hasNext()) {
+                        Map.Entry pair = itr1.next();
+                        if(pair.getKey().toString().equalsIgnoreCase("duration")) {
+                            duration = Integer.parseInt(pair.getValue().toString());
+                        }
+                        if(pair.getKey().toString().equalsIgnoreCase("skill")) {
+                            skill = pair.getValue().toString();
+                        }
+
+                    }
+                    allSkills = allSkills + skill;
+                    String skills = skill+"_FIELD";
+                    System.out.println("SKill with DUration-->>" + skills + " " + duration);
+                    document.add(new IntField(skills, duration ,Field.Store.YES));
+
+                    document.add(new StringField("allSkills", allSkills, Field.Store.YES));
+                    writer.addDocument(document);
+                }
+                fr.close();
+            }
+            catch (Exception e) {
                 e.printStackTrace();
             }
-            finally {
-                try {
-                    fr.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
+
+
+
         });
         int newNumDocs = writer.numDocs();
 
@@ -77,36 +100,51 @@ public class ScoringEngine {
             LOGGER.log(Level.INFO,"{} documents get indexed",OriginalNumDocs-newNumDocs);
         }
         queue.clear();
+        writer.commit();
         writer.close();
 
 
     }
 
-    public List<Document> searchQuery(String userQuery) throws IOException {
+    public Set<String> searchQuery(String userQuery) throws IOException {
         IndexReader reader = DirectoryReader.open(FSDirectory.open(new File(getClass().getResource("/CORPUS").getFile())));
         IndexSearcher searcher = new IndexSearcher(reader);
+
+        Query query = NumericRangeQuery.newIntRange("Java_FIELD",5,50,true,true);
+        Query query2 = NumericRangeQuery.newIntRange("Kafka_FIELD", 1,30,true,true);
+        Query query3 = NumericRangeQuery.newIntRange("Angular_FIELD", 10,100,true,true);
+        query.setBoost((float) 2.0);
+
+        BooleanQuery booleanQuery = new BooleanQuery();
+        booleanQuery.add(query, BooleanClause.Occur.SHOULD);
+        booleanQuery.add(query2, BooleanClause.Occur.SHOULD);
+        booleanQuery.add(query3, BooleanClause.Occur.SHOULD);
+
+
         TopScoreDocCollector collector = null;
-        List<Document> resultDocList = new ArrayList<>();
+        HashSet<String> resultset = new LinkedHashSet<>();
 
         try {
-            Query q = new QueryParser(Version.LUCENE_47, "contents", analyzer).parse(userQuery);
-            collector = TopScoreDocCollector.create(1000,true); //Scoring for all the documents.
-            searcher.search(q, collector);
+            collector = TopScoreDocCollector.create(100,true); //Scoring for all the documents.
+            searcher.search(booleanQuery, collector);
             ScoreDoc[] hits = collector.topDocs().scoreDocs;
 
-            for (int i = 0; i < Math.min(100, hits.length); ++i) {
+            for (int i = 0; i < Math.min(50, hits.length); ++i) {
                 int docId = hits[i].doc;
                 Document d = searcher.doc(docId);
-                resultDocList.add(d);
-                String filename = d.get("filename");
-                System.out.println("Filename--->>>" + filename + "Score-->>>" + hits[i].score);
+                resultset.add(d.get("location"));
+                String location = d.get("location");
+                System.out.println("File location--->>>" + location + " Score-->>>" + hits[i].score);
             }
+            resultset.forEach(doc -> {
+                System.out.println("New location--->>>" + doc);
+            });
         }
         catch (Exception e) {
          e.printStackTrace();
         }
-        System.out.println("Result Document-->>" + resultDocList);
-        return resultDocList;
+        System.out.println("Result Document-->>" + resultset);
+        return resultset;
     }
 
     private void addFiles(File file) {
@@ -123,7 +161,7 @@ public class ScoringEngine {
             // ===================================================
             // Only index text files
             // ===================================================
-            if (filename.endsWith(".txt")) {
+            if (filename.endsWith(".json")) {
                 queue.add(file);
             } else {
                 System.out.println("Skipped " + filename);
@@ -134,9 +172,7 @@ public class ScoringEngine {
    public static void main(String args[]) throws IOException {
        ScoringEngine scoringEngine = new ScoringEngine();
        scoringEngine.indexFilesDirectory();
-       scoringEngine.searchQuery("java \n");
+       scoringEngine.searchQuery("Java");
    }
-
-
-
+   
 }
